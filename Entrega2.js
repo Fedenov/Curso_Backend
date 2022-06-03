c85b0e7e17565ec568057b826da579d9d98b4b80
Fedenov <elfeede_n@hotmail.com>
2022-06-02 23:25:22 -0300

    entrega

--- Entrega2.js
+++ Entrega2.js
@@ -1,40 +1,62 @@
+const fs = require("fs");
+
 class Contenedor {
-    constructor(nombreArchivo){
-        const fs = require("fs")
-        this.archivo = nombreArchivo
-        this.ruta = `./${nombreArchivo}.txt`
-        console.log(this.ruta)
-        fs.writeFileSync(this.ruta, "")
+  constructor(fileName) {
+    this.fileName = fileName;
+    fs.promises.writeFile(`./${fileName}`, "");
+  }
+  async save(objeto) {
+    let data = await fs.promises.readFile(`./${this.fileName}`, "utf-8");
+    if (!data) {
+      objeto.id = 1;
+      const arr = [objeto];
+      await fs.promises.writeFile(`./${this.fileName}`, JSON.stringify(arr));
+      return objeto.id;
+    } else {
+      data = JSON.parse(data);
+      objeto.id = data.length + 1;
+      data.push(objeto);
+      await fs.promises.writeFile(`./${this.fileName}`, JSON.stringify(data));
+      return objeto.id;
     }
-    async save(newObjeto){
-        const fs = require("fs")
-        let info = await fs.promises.readFile(this.ruta, 'utf8')
-        if (info){
-            info = JSON.parse(info)
-            newObjeto.ID = info.length + 1
-            info.push(newObjeto)
-            await fs.promises.writeFile(this.ruta, JSON.stringify(info))
-            console.log("positivo")
-        } else {
-            newObjeto.ID = 1
-            const obj = [newObjeto]
-            await fs.promises.writeFile(this.ruta, JSON.stringify(obj))
-            console.log("negativo")
-        }
-        /*let newID = lastID++
-        console.log(newID)
-        const objeto = [{ID: newID, objeto: newObjeto}]
-        fs.appendFileSync(this.ruta, `${JSON.stringify(objeto)}\n`)*/
-        return newObjeto.ID
-        return(`ID: ${lastID+1}`)
+  }
+  async getById(id) {
+    let productos = JSON.parse(
+      await fs.promises.readFile("./productos.txt", "utf-8")
+    );
+    let objeto = productos.find((prod) => prod.id == id);
+    console.log(objeto ? objeto : "ese ID no existe");
+  }
+  async getAll() {
+    let productos = JSON.parse(
+      await fs.promises.readFile("./productos.txt", "utf-8")
+    );
+    console.log(productos);
+  }
+  async deleteById(id) {
+    let productos = JSON.parse(
+      await fs.promises.readFile("./productos.txt", "utf-8")
+    );
+    if (productos.some((prod) => prod.id == id)) {
+      let newProductos = productos.filter((prod) => prod.id != id);
+      await fs.promises.writeFile(
+        `./${this.archivo}`,
+        JSON.stringify(newProductos)
+      );
+      console.log("El producto ha sido eliminado");
+    } else {
+      console.log("No existe producto ID ingresado");
     }
+  }
+  async deleteAll() {
+    await fs.promises.writeFile(`./${this.archivo}`, "[]");
+    console.log("Todos los archivos han sido eliminados");
+  }
 }
 
-let contenedor = new Contenedor("PruebaContenedor")
-/*console.log(contenedor.save("32"))
-console.log(contenedor.save("40"))*/
-
-contenedor.save({objeto: "shampoo"}).then(ID => {
-    console.log(ID)
-    contenedor.save({objeto: "acond"}).then(ID2 => console.log(ID2))
-})
+const productos = new Contenedor("productos");
+productos.save({ name: "Lapicera" }).then((ID) => {
+  console.log(ID);
+  productos.save({ name: "Regla" }).then((ID2) => console.log(ID2));
+  productos.getById(1);
+});
