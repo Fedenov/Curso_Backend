c1e6f6f804197d31c626b318ca18e9a2ec2f6f48
Fedenov <elfeede_n@hotmail.com>
2022-06-23 23:27:38 -0300

    entrega5

---
+++ Semana5/Entrega5/hbs/routes/index.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+const router = Router();
+const productos = require("./products");
+
+router.get("/", (req, res) => {
+    res.render("index");
+});
+
+router.use("/productos", productos);
+
+module.exports = router;
