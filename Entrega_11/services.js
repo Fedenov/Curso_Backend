49ef115ece8b0230e89d9ded691787692bac8388
FNov <85361784+Fedenov@users.noreply.github.com>
2022-08-11 21:28:08 -0300

    Add files via upload

---
+++ Entrega_11/services.js
@@ -0,0 +1,9 @@
+function auth(req, res, next) {
+    if (req.session.admin === true) {
+        next();
+    } else {
+        res.sendFile(path.join(__dirname, "../public/login.html"));
+    }
+}
+
+export { auth };
