a3188976c7512679c8e8bd775da299157fc2db69
FNov <85361784+Fedenov@users.noreply.github.com>
2022-10-20 20:24:47 -0300

    Add files via upload

---
+++ Reforma_GraphQL_Entrega_18/controllers/user.controller.js
@@ -0,0 +1,9 @@
+const logOut = (req, res) => {
+    let user = req.session.user;
+    req.session.destroy();
+    res.render("bye", { data: user });
+};
+
+export default {
+    logOut,
+};
---
+++ Reforma_GraphQL_Entrega_18/models.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+    password: String,
+    username: String,
+    firstName: String,
+    lastName: String,
+    avatar: String,
+    admin: Boolean,
+});
+
+const cart_schema = new mongoose.Schema({
+    user_id: String,
+    products: Array,
+});
+
+const products_schema = new mongoose.Schema({
+    product_name: String,
+    product_description: String,
+    product_price: Number,
+    product_imgUrl: String,
+    product_stock: Number,
+});
+
+const chat_schema = new mongoose.Schema({
+    author: {
+        email: { type: String, required: true },
+        nombre: { type: String, required: true },
+        apellido: { type: String, required: true },
+        alias: { type: String, required: true },
+    },
+    text: { type: String, required: true },
+    fechahora: { type: String, required: true },
+});
+
+const modelo = mongoose.model("User", schema);
+const cart_model = mongoose.model("Cart", cart_schema);
+const products_model = mongoose.model("Products", products_schema);
+const chat_model = mongoose.model("Chats", chat_schema);
+
+export { modelo, cart_model, products_model, chat_model };
