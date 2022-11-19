7fd30efeb5608ac26d0ed751ecae920930270a79
FNov <85361784+Fedenov@users.noreply.github.com>
2022-11-19 00:24:38 -0300

    Delete Ultima_Entrega directory

--- Ultima_Entrega/DAOs/DAO_pedidos.js
+++
@@ -1,9 +0,0 @@
-import { order_model } from "../modelos.js";
-
-const newOrder = async (orden) => {
-    const newOrder = await order_model.create(orden)
-}
-
-export const orderDao = {
-    newOrder
-}
